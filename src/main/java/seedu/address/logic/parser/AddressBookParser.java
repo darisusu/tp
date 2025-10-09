7303e73a058ac053f12cf28ad9d78cbec4844e63
Harun <theayyyron@gmail.com>
2025-10-09 19:47:13 +0800

    Create and implement GoalCommandParser

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -79,7 +79,7 @@ public class AddressBookParser {
             return new HelpCommand();
 
         case GoalCommand.COMMAND_WORD:
-            return new GoalCommand();
+            return new GoalCommandParser().parse(arguments);
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
---
+++ src/main/java/seedu/address/logic/parser/GoalCommandParser.java
@@ -0,0 +1,38 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.GoalCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_GOAL;
+
+/**
+ * Parses input arguments and creates a new {@code GoalCommand} object
+ */
+public class GoalCommandParser implements Parser<GoalCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the {@code RemarkCommand}
+     * and returns a {@code GoalCommand} object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public GoalCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_GOAL);
+
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (IllegalValueException ive) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, GoalCommand.MESSAGE_USAGE), ive);
+        }
+
+        String goal = argMultimap.getValue(PREFIX_GOAL).orElse("");
+
+        return new GoalCommand(index, goal);
+    }
+
+}
