9e7074fd06896476f55c9e2d7cb8a18aa350b2d0
Harun <theayyyron@gmail.com>
2025-10-09 19:32:12 +0800

    Enable GoalCommand to throw exception

--- src/main/java/seedu/address/logic/commands/GoalCommand.java
+++ src/main/java/seedu/address/logic/commands/GoalCommand.java
@@ -1,12 +1,27 @@
 package seedu.address.logic.commands;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 public class GoalCommand extends Command {
+
     public static final String COMMAND_WORD = "goal";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Edits the goal of the person identified "
+            + "by the index number used in the last person listing. "
+            + "Existing goal will be overwritten by the input.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + "goal/ [goal]\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + "goal/ Lose 10kg.";
+
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET =
+            "Goal command not implemented yet";
+
+
     @Override
-    public CommandResult execute(Model model) {
-        return new CommandResult("Hello from goal");
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
     }
 }
