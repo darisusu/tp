ff9e4a753a9de7b420053516e0f4eb951abc4933
Harun <theayyyron@gmail.com>
2025-10-09 20:39:26 +0800

    Update GoalCommand

--- src/main/java/seedu/address/logic/commands/GoalCommand.java
+++ src/main/java/seedu/address/logic/commands/GoalCommand.java
@@ -1,11 +1,16 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.List;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Goal;
+import seedu.address.model.person.Person;
 
 public class GoalCommand extends Command {
 
@@ -21,6 +26,9 @@ public class GoalCommand extends Command {
             + "goal/ Lose 10kg.";
 
     public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Remark: %2$s";
+
+    public static final String MESSAGE_ADD_GOAL_SUCCESS = "Added remark to Person: %1$s";
+    public static final String MESSAGE_DELETE_GOAL_SUCCESS = "Removed remark from Person: %1$s";
 
     private final Index index;
     private final Goal goal;
@@ -38,8 +46,31 @@ public class GoalCommand extends Command {
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        throw new CommandException(
-                String.format(MESSAGE_ARGUMENTS, index.getOneBased(), goal));
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToEdit = lastShownList.get(index.getZeroBased());
+        Person editedPerson = new Person(
+                personToEdit.getName(), personToEdit.getPhone(), personToEdit.getEmail(),
+                personToEdit.getAddress(), goal, personToEdit.getTags());
+
+        model.setPerson(personToEdit, editedPerson);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
+        return new CommandResult(generateSuccessMessage(editedPerson));
+    }
+
+    /**
+     * Generates a command execution success message based on whether
+     * the goal is added to or removed from
+     * {@code personToEdit}.
+     */
+    private String generateSuccessMessage(Person personToEdit) {
+        String message = !goal.value.isEmpty() ? MESSAGE_ADD_GOAL_SUCCESS : MESSAGE_DELETE_GOAL_SUCCESS;
+        return String.format(message, Messages.format(personToEdit));
     }
 
     @Override
