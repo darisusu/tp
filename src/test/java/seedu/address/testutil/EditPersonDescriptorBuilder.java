8566045cf3628d7c3b4fbced72c9de47da928bbc
Darius Deng <dariusdengyang@gmail.com>
2025-10-15 02:36:58 +0800

    Add weight to EditpersonDescriptorBuilder

--- src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
+++ src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
@@ -6,10 +6,14 @@ import java.util.stream.Stream;
 
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.model.person.Address;
+import seedu.address.model.person.Age;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.Gender;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
+import seedu.address.model.person.Height;
+import seedu.address.model.person.Weight;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -36,6 +40,8 @@ public class EditPersonDescriptorBuilder {
         descriptor.setPhone(person.getPhone());
         descriptor.setEmail(person.getEmail());
         descriptor.setAddress(person.getAddress());
+        descriptor.setHeight(person.getHeight());
+        descriptor.setWeight(person.getWeight());
         descriptor.setTags(person.getTags());
     }
 
@@ -72,6 +78,33 @@ public class EditPersonDescriptorBuilder {
     }
 
     /**
+     * Sets the {@code Height} of the {@code EditPersonDescriptor} that we are building.
+     */
+    public EditPersonDescriptorBuilder withHeight(String height) {
+        descriptor.setHeight(new Height(height));
+        return this;
+    }
+
+    /**
+     * Sets the {@code Weight} of the {@code EditPersonDescriptor} that we are building.
+     * Must always be used after setting height.
+     */
+    public EditPersonDescriptorBuilder withWeight(String weight) {
+        descriptor.setWeight(new Weight(weight));
+        return this;
+    }
+
+    public EditPersonDescriptorBuilder withAge(String age) {
+        descriptor.setAge(new Age(age));
+        return this;
+    }
+
+    public EditPersonDescriptorBuilder withGender(String gender) {
+        descriptor.setGender(new Gender(gender));
+        return this;
+    }
+
+    /**
      * Parses the {@code tags} into a {@code Set<Tag>} and set it to the {@code EditPersonDescriptor}
      * that we are building.
      */
@@ -80,6 +113,7 @@ public class EditPersonDescriptorBuilder {
         descriptor.setTags(tagSet);
         return this;
     }
+
 
     public EditPersonDescriptor build() {
         return descriptor;
