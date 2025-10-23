179ba0f53305892eb1f8f8a26e3712610362d904
Harun <theayyyron@gmail.com>
2025-10-23 21:12:32 +0800

    Import FXML into Dashboard

--- src/main/java/seedu/address/ui/DashboardPanel.java
+++ src/main/java/seedu/address/ui/DashboardPanel.java
@@ -1,20 +1,22 @@
 package seedu.address.ui;
 
+import javafx.fxml.FXML;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 
 public class DashboardPanel extends UiPart<Region> {
-    private static final String FXML = "DashboardPanel.fxml";
 
-    public DashboardPanel() {
-        super(FXML);
-    }
+    private static final String FXML = "DashboardPanel.fxml";
 
     @FXML
     private VBox leftColumn;
 
     @FXML
     private VBox rightColumn;
+
+    public DashboardPanel() {
+        super(FXML);
+    }
 
     public VBox getLeftColumn() {
         return leftColumn;
