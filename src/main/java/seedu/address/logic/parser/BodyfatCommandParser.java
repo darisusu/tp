3406b03e2d6ab0f26646926c4c728a703098a9f1
Harun <theayyyron@gmail.com>
2025-10-30 00:49:54 +0800

    Fix bodyfat bug
    Fix error where invalid input for bodyfat did not generate
    valid error messages. Errors were in the specific parser class

--- src/main/java/seedu/address/logic/parser/BodyfatCommandParser.java
+++ src/main/java/seedu/address/logic/parser/BodyfatCommandParser.java
@@ -3,12 +3,15 @@ package seedu.address.logic.parser;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_BODYFAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DEADLINE;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.BodyfatCommand;
+import seedu.address.logic.commands.DeadlineCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Bodyfat;
+import seedu.address.model.person.Deadline;
 
 /**
  * Parses input arguments and creates a new {@code BodyfatCommand} object.
@@ -24,12 +27,22 @@ public class BodyfatCommandParser implements Parser<BodyfatCommand> {
     public BodyfatCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_BODYFAT);
-
         Index index;
+
         try {
             index = ParserUtil.parseIndex(argMultimap.getPreamble());
         } catch (IllegalValueException ive) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, BodyfatCommand.MESSAGE_USAGE), ive);
+        }
+
+        if (!argMultimap.getValue(PREFIX_BODYFAT).isPresent()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    BodyfatCommand.MESSAGE_USAGE));
+        }
+
+        String raw = argMultimap.getValue(PREFIX_BODYFAT).get();
+        if (!Bodyfat.isValidBodyfat(raw)) {
+            throw new ParseException(Bodyfat.MESSAGE_CONSTRAINTS);
         }
 
         String bodyfatValue = argMultimap.getValue(PREFIX_BODYFAT).orElse("");
